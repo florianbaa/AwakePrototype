b7483d00846da5fcf45c11d76c58e7967674b289
LeonMinke <60382786+LeonMinke@users.noreply.github.com>
2020-01-28 15:58:08 +0100

    end of day upload
    see title

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -9,6 +9,7 @@ public class EnemyBehaviour : MonoBehaviour
     public float movespeed = 20;
     public float scandistance = 15f;
     float enemyDistance;
+    float groundDistance;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +19,16 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        enemyDistance = Vector3.Distance(GameObject.FindWithTag("enemy").transform.position, GameObject.FindWithTag("Player").transform.position); ;
+        enemyDistance = Vector3.Distance(GameObject.FindWithTag("enemy").transform.position, GameObject.FindWithTag("Player").transform.position);
+        //groundDistance = Vector3.Distance(GameObject.FindWithTag("ground").transform.position, GameObject.FindWithTag("Player").transform.position);
+        /*{
+            if (groundDistance < 2)
+            {
+                transform.up;
+            }
+        }*/
+
+
 
         if (enemyDistance < 20)
         {
@@ -35,6 +45,7 @@ public class EnemyBehaviour : MonoBehaviour
         }
 
     }
+    
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
