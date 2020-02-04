d58d8a4870e78942977b472ba2757b6cf796cecc
Leon M'laiel <studio@lmdinteractive.com>
2020-02-04 10:23:29 +0100

    Restart Menu + Main Menu Fix

---
+++ Assets/MainMenu/RestartControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartControl : MonoBehaviour
+{
+    public void ButtonRestart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ButtonMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ButtonQuit()
+    {
+        Application.Quit();
+    }
+}
