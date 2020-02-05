9f4bcdc226f61bc45ba1aaf078b9cd652c315177
Leon M'laiel <studio@lmdinteractive.com>
2020-02-05 13:09:08 +0100

    Drone Explosion / Death

---
+++ Assets/Scripts/DroneExplosion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneExplosion : MonoBehaviour
+{
+    public float TimeToLive = 5f;
+    private void Start()
+    {
+        Destroy(gameObject, TimeToLive);
+    }
+}
